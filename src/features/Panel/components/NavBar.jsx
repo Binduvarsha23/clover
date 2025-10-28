2dc239aa308afd9ea9b4a11c18bff3fae16f6e2d
Binduvarsha23 <123391226+Binduvarsha23@users.noreply.github.com>
2025-10-28 22:07:47 +0530

    Update NavBar.jsx

--- src/features/Panel/components/NavBar.jsx
+++ src/features/Panel/components/NavBar.jsx
@@ -7,31 +7,50 @@ import {
 function NavBar() {
   const [nav, setNav] = useGlobal('nav');
 
+  const getItemStyle = (key) => ({
+    color: nav === key ? 'black' : '#888',
+    cursor: 'pointer',
+  });
+
   return (
     <div className="nav-bar uk-flex">
-      <div className={`item${nav === 'rooms' ? ' active' : ''}`} onClick={() => setNav('rooms')}>
+      <div
+        className={`item${nav === 'rooms' ? ' active' : ''}`}
+        onClick={() => setNav('rooms')}
+        style={getItemStyle('rooms')}
+      >
         <div className="icon">
           <FiMessageCircle />
         </div>
         <div className="text">Rooms</div>
       </div>
-      <div className={`item${nav === 'search' ? ' active' : ''}`} onClick={() => setNav('search')}>
+
+      <div
+        className={`item${nav === 'search' ? ' active' : ''}`}
+        onClick={() => setNav('search')}
+        style={getItemStyle('search')}
+      >
         <div className="icon">
           <FiSearch />
         </div>
         <div className="text">Search</div>
       </div>
-      <div className={`item${nav === 'favorites' ? ' active' : ''}`} onClick={() => setNav('favorites')}>
+
+      <div
+        className={`item${nav === 'favorites' ? ' active' : ''}`}
+        onClick={() => setNav('favorites')}
+        style={getItemStyle('favorites')}
+      >
         <div className="icon">
           <FiStar />
         </div>
         <div className="text">Favorites</div>
       </div>
+
       <div
         className={`item${nav === 'meetings' ? ' active' : ''}`}
-        onClick={() => {
-          setNav('meetings');
-        }}
+        onClick={() => setNav('meetings')}
+        style={getItemStyle('meetings')}
       >
         <div className="icon">
           <FiUsers />
